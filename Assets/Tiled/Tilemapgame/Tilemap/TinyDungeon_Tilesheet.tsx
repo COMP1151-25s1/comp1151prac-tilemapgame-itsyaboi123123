85718a29922564c77a1a90ed055d09a1fe13d6cc
itsyaboi123123 <benjamin.gallard@students.mq.edu.au>
2025-05-06 10:57:00 +1000

    Week 9

---
+++ Assets/Tiled/Tilemapgame/Tilemap/TinyDungeon_Tilesheet.tsx
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TinyDungeon_Tilesheet" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
+ <properties>
+  <property name="unity:IsTrigger" value="true"/>
+  <property name="unity:layer" value="Hazard"/>
+ </properties>
+ <image source="tilemap.png" width="203" height="186"/>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+   <object id="3" x="0" y="0" width="16" height="16"/>
+   <object id="4" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="121">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="16" height="15"/>
+   <object id="2" x="0" y="1" width="16" height="15"/>
+   <object id="3" x="0" y="1" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+</tileset>
